6eda633c4a6cc58797c83018e143e88743ee4369
ulyana141124-cmd <ulyana14.11.24@gmail.com>
2026-03-16 13:57:08 +0300

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple {
     public static void main(String args[]) {
-        System.out.println("Андриевская Ульяна 13.03.2026");
+        System.out.println("Андриевская Ульяна 23-ВС 13.03.2026");
     }
 }
